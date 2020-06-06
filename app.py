89f4c1444038e6feebfcfc7f8d58ede81247e7ef
mauroindigne <mauroindigne@gmail.com>
2020-06-06 10:58:22 +0000

    cleaned up base.html and added stunt-videos.html

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
+if os.path.exists("env.py"):
+    import env
+
 app = Flask(__name__)
-app.config['MONGO_DBNAME'] = 'stunt-lounge'
-app.config["MONGO_URI"] = 'mongodb+srv://Kincomoro5:Kincomoro5@cluster0-eyd41.mongodb.net/stunt_lounge?retryWrites=true&w=majority'
+
+app.config["MONGO_URI"] = os.environ = 'SECRET_KEY'
 
 
 mongo = PyMongo(app)
@@ -13,6 +16,10 @@ mongo = PyMongo(app)
 
 @app.route('/base.html')
 
+@app.route('/home')
+def get_tasks():
+    return render_template("base.html", 
+                           tasks=mongo.db.tasks.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
